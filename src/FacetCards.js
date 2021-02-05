1f466bcb3b94827b444f1f07549e1fe1804992d0
Amit Verma <akv004@gmail.com>
2021-02-05 17:15:17 -0600

    fix each facet

--- src/FacetCards.js
+++ src/FacetCards.js
@@ -2,31 +2,33 @@ import React, { useState } from "react";
 
 export const FacetCards = (props) => {
   const { searchResults } = props;
+  return searchResults.facets.map((facet) => {
+    return <Facet facet={facet} />;
+  });
+};
+
+export const Facet = (props) => {
+  const { facet } = props;
   const [show, setShow] = useState("facet__hidden");
   const [moreOrLess, setMoreOrLess] = useState("More");
-
   function toggleFilter() {
     setShow(show === "facet__hidden" ? "facet__section" : "facet__hidden");
     setMoreOrLess(moreOrLess === "More" ? "Less" : "More");
   }
-
-  const showfewItem = (index, minItemToShow, show) => {
+  const showFew = (index, minItemToShow, show) => {
     if (index < minItemToShow) {
       return "facet__section";
     } else {
       return show;
     }
   };
-
-  return searchResults.facets.map((facet) => {
-    return (
-      <div>
-        <h4>{facet.displayName}</h4>
-        {facet.values.map((value, ind) => (
-          <div className={showfewItem(ind, 2, show)}>{value.value}</div>
-        ))}
-        <button onClick={toggleFilter}>{moreOrLess}</button>
-      </div>
-    );
-  });
+  return (
+    <div>
+      <h4>{facet.displayName}</h4>
+      {facet.values.map((value, ind) => (
+        <div className={showFew(ind, 2, show)}>{value.value}</div>
+      ))}
+      <button onClick={toggleFilter}>{moreOrLess}</button>
+    </div>
+  );
 };
