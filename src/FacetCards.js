9fec572f1e15976863e470c8a03fabb5b17c28c5
Amit Verma <akv004@gmail.com>
2021-02-05 16:51:04 -0600

    updated

--- src/FacetCards.js
+++ src/FacetCards.js
@@ -2,14 +2,30 @@ import React, { useState } from "react";
 
 export const FacetCards = (props) => {
   const { searchResults } = props;
-  const [filter, setFilter] = useState([]);
+  const [show, setShow] = useState("facet__hidden");
+  const [moreOrLess, setMoreOrLess] = useState("More");
+
+  function toggleFilter() {
+    setShow(show === "facet__hidden" ? "facet__section" : "facet__hidden");
+    setMoreOrLess(moreOrLess === "More" ? "Less" : "Less");
+  }
+
+  const showfewItem = (index, minItemToShow, show) => {
+    if (index < minItemToShow) {
+      return "facet__section";
+    } else {
+      return show;
+    }
+  };
+
   return searchResults.facets.map((facet) => {
     return (
       <div>
         <h4>{facet.displayName}</h4>
         {facet.values.map((value, ind) => (
-          <div>{value.value}</div>
+          <div className={showfewItem(ind, 2, show)}>{value.value}</div>
         ))}
+        <button onClick={toggleFilter}>{moreOrLess}</button>
       </div>
     );
   });
